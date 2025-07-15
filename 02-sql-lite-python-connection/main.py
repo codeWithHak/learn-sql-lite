f40edbbb087bbb278c54def4fea544ba23052f87
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-07-15 13:00:56 +0500

    add comments

--- 02-sql-lite-python-connection/main.py
+++ 02-sql-lite-python-connection/main.py
@@ -7,18 +7,27 @@ connection = sqlite3.connect(r"D:\huzair\coding\learn-sql-lite\02-sql-lite-pytho
 # create a cursor to interact with database
 cursor = connection.cursor()
 
+# query to create table
 create_query = "create table employees (id,name,position,salary)"
 
+# query to insert data
 insert_query = "insert into employees (id,name,position,salary) values(1,'Huzair','Intern',20000), (2,'Nasir','Intern',20000)"
 
+# query to get data
 select_query = "select * from employees"
 
+# query to delete data
 delete_query = "delete from employees where id = 2"
+
+
 # cursor.execute(insert_query)
 
-# it saves the data after insertion (updates db)
 
+
+# executing querry
 cursor.execute(insert_query)
+
+# it saves the data after insertion (updates db)
 connection.commit()
 
 cursor.execute(select_query)
